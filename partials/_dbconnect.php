ffffb11b96e136532b86934592820d08733283b7
Shibli <ahmedrazashibli@gmail.com>
2023-01-08 20:25:00 +0530

    Establishing connection with the database

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,3 +1,16 @@
 <?php
 
+// Variables for the connection
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "macs";
+
+// Making the connection with the database
+$conn = mysqli_connect($server, $user, $pass, $db);
+
+// Checiking the connection
+if(!$conn){
+    echo "Error while connecting with the database". mysqli_connect_error();
+}
 ?>
