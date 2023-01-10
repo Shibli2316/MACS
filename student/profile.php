efe89689d6f2f611a99bdccb76175e3d1613f8c0
Shibli <ahmedrazashibli@gmail.com>
2023-01-10 09:51:43 +0530

    Adding redirect link

--- student/profile.php
+++ student/profile.php
@@ -17,41 +17,56 @@ include "../partials/_dbconnect.php"
 </head>
 
 <body>
-    <?php 
-    require "../partials/_nav.php"; 
+    <?php
+    require "../partials/_nav.php";
     $sql = "SELECT * FROM `students` WHERE username = '$user'";
     $result = mysqli_query($conn, $sql);
-    $details= mysqli_fetch_assoc($result);
-    if($details['f_name'] == ""){
+    $details = mysqli_fetch_assoc($result);
+    if ($details['f_name'] == "") {
         echo "Enter name";
     }
-    if($_SERVER['REQUEST_METHOD']=="POST"){
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $f_name = $_POST['f_name'];
         $l_name = $_POST['l_name'];
         $email = $_POST['email'];
         $exam = $_POST['exam'];
         $rank = $_POST['rank'];
-           
+
         $updatingDetails = "UPDATE `students` SET `f_name` = '$f_name', `l_name` = '$l_name', `email` = '$email', `exam` = '$exam', `rank` = '$rank' WHERE `students`.`username` = '$user';";
         $run = mysqli_query($conn, $updatingDetails);
-        if(!$run){
+        if (!$run) {
             echo "Error while updating records";
+        } else {
+            echo "<script>alert('Your records has been updated successfully!!!')</script>";
+    ?>
+            <meta http-equiv="refresh" content="0; url = profile.php" />
+    <?php
         }
     }
     ?>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" class="mx-2 my-2" method="post">
         <label for="name">First Name</label>
-        <input type="text" name="f_name" id="f_name" placeholder="<?php if($details['f_name']==""){echo "Enter name";}else{echo $details['f_name'];}?>"> <br> <hr>
+        <input type="text" name="f_name" id="f_name" placeholder="<?php if ($details['f_name'] == "") {
+                                                                        echo "Enter name";
+                                                                    } else {
+                                                                        echo $details['f_name'];
+                                                                    } ?>"> <br>
+        <hr>
         <label for="name">Last Name</label>
-        <input type="text" name="l_name" id="l_name"> <br> <hr>
+        <input type="text" name="l_name" id="l_name"> <br>
+        <hr>
         <label for="name">Username</label>
-        <input type="text" name="u_name" id="u_name" readonly placeholder="<?php echo $details['username'];?>"> <br> <hr>
+        <input type="text" name="u_name" id="u_name" readonly placeholder="<?php echo $details['username']; ?>"> <br>
+        <hr>
         <label for="email">Email</label>
-        <input type="email" name="email" id="email"> <br> <hr>
+        <input type="email" name="email" id="email"> <br>
+        <hr>
         <label for="name">Exam</label>
-        <input type="text" name="exam" id="exam"> <br> <hr>
+        <input type="text" name="exam" id="exam"> <br>
+        <hr>
         <label for="name">Rank</label>
-        <input type="text" name="rank" id="rank"> <br> <hr>
+        <input type="text" name="rank" id="rank"> <br>
+        <hr>
         <input type="submit" value="Save">
     </form>
 
